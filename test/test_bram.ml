82740c7315a5ccef42e8d6e3637968748178e725
Mark Devenyi <markdevenyidev@gmail.com>
2025-12-29 19:00:30 -0500

    test(ram): add `Cyclesim` testbench for BRAM module

---
+++ test/test_bram.ml
@@ -0,0 +1,74 @@
+open Base
+open Hardcaml
+open Hardcaml_waveterm
+open Solution.Bram
+
+let testbench () =
+  let data_width = 32 in
+  let bram_depth = 256 in
+  let scope = Scope.create ~flatten_design:true () in
+  let module TestBRAM =
+    Make_BRAM (struct
+      let data_width = data_width
+      let depth = bram_depth
+    end)
+  in
+  let addr_width = Int.ceil_log2 bram_depth in
+  let module Sim = Cyclesim.With_interface (TestBRAM.I) (TestBRAM.O) in
+  let sim = Sim.create (TestBRAM.create scope) in
+  let waves, sim = Waveform.create sim in
+  let inputs = Cyclesim.inputs sim in
+  let outputs = Cyclesim.outputs sim in
+  (* TODO: There has to be a built-in function for this... *)
+  let bits_of_string_be ~width (s : string) : Bits.t =
+    Bits.of_int
+      ~width
+      ((fun s -> String.fold s ~init:0 ~f:(fun acc c -> (acc lsl 8) lor Char.to_int c)) s)
+  in
+  let read ~addr =
+    inputs.read_enable := Bits.vdd;
+    inputs.read_addr := addr |> Bits.of_int ~width:addr_width
+  in
+  let write ~addr ~data =
+    inputs.write_enable := Bits.vdd;
+    inputs.write_addr := addr |> Bits.of_int ~width:addr_width;
+    inputs.write_data := data
+  in
+  write ~addr:0x1 ~data:(bits_of_string_be ~width:data_width "Hell");
+  read ~addr:0x1;
+  Cyclesim.cycle sim;
+  (* Write is done *)
+  Cyclesim.cycle sim;
+  (* Read is done *)
+  write ~addr:0x2 ~data:!(outputs.read_data);
+  Cyclesim.cycle sim;
+  waves
+;;
+
+let%expect_test "Memory works correctly" =
+  let waves = testbench () in
+  Waveform.print ~wave_width:4 waves;
+  [%expect
+    {|
+┌Signals────────┐┌Waves──────────────────────────────────────────────┐
+│clock          ││┌────┐    ┌────┐    ┌────┐    ┌────┐    ┌────┐    ┌│
+│               ││     └────┘    └────┘    └────┘    └────┘    └────┘│
+│               ││──────────────────────────────                     │
+│read_addr      ││ 01                                                │
+│               ││──────────────────────────────                     │
+│read_enable    ││──────────────────────────────                     │
+│               ││                                                   │
+│               ││────────────────────┬─────────                     │
+│write_addr     ││ 01                 │02                            │
+│               ││────────────────────┴─────────                     │
+│               ││──────────────────────────────                     │
+│write_data     ││ 48656C6C                                          │
+│               ││──────────────────────────────                     │
+│write_enable   ││──────────────────────────────                     │
+│               ││                                                   │
+│               ││────────────────────┬─────────                     │
+│read_data      ││ 00000000           │48656C6C                      │
+│               ││────────────────────┴─────────                     │
+└───────────────┘└───────────────────────────────────────────────────┘
+  |}]
+;;
