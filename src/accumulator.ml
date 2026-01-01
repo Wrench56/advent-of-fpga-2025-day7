978b91cacd2098ef9be2d68ac846a3600831ad75
Mark Devenyi <markdevenyidev@gmail.com>
2026-01-01 03:17:31 -0500

    feat(accu): add optionally saturating accumulator module

---
+++ src/accumulator.ml
@@ -0,0 +1,49 @@
+open Base
+open Hardcaml
+
+module Make_Accumulator (Config : sig
+    val max_value : int
+    val add_width : int
+    val saturating : bool
+  end) =
+struct
+  let max_value = Int.max 1 Config.max_value
+  let reg_size = max_value + 1 |> Int.ceil_log2
+
+  module I = struct
+    type 'a t =
+      { clock : 'a
+      ; clear : 'a
+      ; enable : 'a
+      ; add : 'a [@bits Config.add_width]
+      }
+    [@@deriving hardcaml]
+  end
+
+  module O = struct
+    type 'a t = { sum : 'a [@bits reg_size] } [@@deriving hardcaml]
+  end
+
+  let create (_scope : Scope.t) (i : Signal.t I.t) : Signal.t O.t =
+    let open Hardcaml.Signal in
+    let spec = Reg_spec.create ~clock:i.clock ~clear:i.clear () in
+    let maxv = max_value |> of_int ~width:reg_size in
+    let add_ext = uresize i.add reg_size in
+    let acc =
+      reg_fb spec ~enable:i.enable ~width:reg_size ~f:(fun acc ->
+        let next = acc +: add_ext in
+        (* TODO: Is saturating logic too expensive? *)
+        if Config.saturating
+        then (
+          let room = maxv -: add_ext in
+          mux2 (acc >: room) maxv next)
+        else next)
+    in
+    { O.sum = acc }
+  ;;
+
+  let hierarchical (scope : Scope.t) (input : Signal.t I.t) =
+    let module H = Hierarchy.In_scope (I) (O) in
+    H.hierarchical ~scope ~name:"accumulator" create input
+  ;;
+end
