95f4d209bb7f53ce0c17a4e6bf34edaff64ce258
Mark Devenyi <markdevenyidev@gmail.com>
2026-01-05 02:24:41 -0500

    test(adder): refactor adder testbench to support the new `AdderN` module

--- test/test_adder.ml
+++ test/test_adder.ml
@@ -3,50 +3,112 @@ open Hardcaml
 open Hardcaml_waveterm
 open Solution.Adder
 
-let testbench add_width =
+let run_adder ~num_inputs ~add_width ~ops =
   let scope = Scope.create ~flatten_design:true () in
-  let module TestAdder =
-    Make_Adder (struct
+  let module TestAdderN =
+    Make_AdderN (struct
+      let num_inputs = num_inputs
       let add_width = add_width
     end)
   in
-  let module Sim = Cyclesim.With_interface (TestAdder.I) (TestAdder.O) in
-  let sim = Sim.create (TestAdder.create scope) in
+  let module Sim = Cyclesim.With_interface (TestAdderN.I) (TestAdderN.O) in
+  let sim = Sim.create (TestAdderN.create scope) in
   let waves, sim = Waveform.create sim in
   let inputs = Cyclesim.inputs sim in
   let cycle n = Utils.cycle sim n in
-  let add num1 num2 =
-    inputs.num1 := Bits.of_int ~width:add_width num1;
-    inputs.num2 := Bits.of_int ~width:add_width num2
+  let drive op =
+    assert (List.length op = num_inputs + 1);
+    for i = 0 to num_inputs - 2 do
+      inputs.nums.(i) := Bits.of_int ~width:add_width (List.nth_exn op i)
+    done;
+    inputs.enable := Bits.of_int ~width:1 (List.last_exn op)
   in
-  add 1 2;
-  cycle 1;
-  let maxval = Int.pow 2 add_width - 1 in
-  add maxval maxval;
-  cycle 1;
-  add 0 0;
-  cycle 1;
-  add maxval 1;
-  cycle 1;
+  List.iter ops ~f:(fun op ->
+    drive op;
+    cycle 1);
   waves
 ;;
 
-let%expect_test "Adder works as expected" =
-  let waves = testbench 16 in
-  Waveform.print ~wave_width:2 ~display_width:35 ~display_height:12 waves;
+let maxval width = Int.pow 2 width - 1
+
+let%expect_test "Adder2 works as expected" =
+  let width = 16 in
+  let mval = maxval width in
+  let en = 1 in
+  let di = 0 in
+  let waves =
+    run_adder
+      ~num_inputs:2
+      ~add_width:16
+      ~ops:
+        [ [ 1; 1; di ]; [ 1; 2; en ]; [ mval; mval; en ]; [ 0; 0; en ]; [ mval; 1; en ] ]
+  in
+  Waveform.print ~wave_width:2 ~display_width:42 ~display_height:17 waves;
   [%expect
     {|
-┌Signal┐┌Waves────────────────────┐
-│      ││──────┬─────┬─────┬───── │
-│num1  ││ 0001 │FFFF │0000 │FFFF  │
-│      ││──────┴─────┴─────┴───── │
-│      ││──────┬─────┬─────┬───── │
-│num2  ││ 0002 │FFFF │0000 │0001  │
-│      ││──────┴─────┴─────┴───── │
-│carry ││      ┌─────┐     ┌───── │
-│      ││──────┘     └─────┘      │
-│      ││──────┬─────┬─────────── │
-│sum   ││ 0003 │FFFE │0000        │
-└──────┘└─────────────────────────┘
+┌Signals─┐┌Waves─────────────────────────┐
+│enable  ││      ┌───────────────────────│
+│        ││──────┘                       │
+│        ││────────────┬─────┬─────┬─────│
+│nums0   ││ 0001       │FFFF │0000 │FFFF │
+│        ││────────────┴─────┴─────┴─────│
+│        ││──────────────────────────────│
+│nums1   ││ 0000                         │
+│        ││──────────────────────────────│
+│carry   ││                              │
+│        ││──────────────────────────────│
+│ready   ││      ┌───────────────────────│
+│        ││──────┘                       │
+│        ││──────┬─────┬─────┬─────┬─────│
+│sum     ││ 0000 │0001 │FFFF │0000 │FFFF │
+│        ││──────┴─────┴─────┴─────┴─────│
+└────────┘└──────────────────────────────┘
+|}]
+;;
+
+let%expect_test "Adder4 works as expected" =
+  let width = 16 in
+  let mval = maxval width in
+  let en = 1 in
+  let di = 0 in
+  let waves =
+    run_adder
+      ~num_inputs:4
+      ~add_width:16
+      ~ops:
+        [ [ 1; 1; 1; 1; di ]
+        ; [ 1; 2; 3; 4; en ]
+        ; [ mval; mval; mval; mval; en ]
+        ; [ 0; 0; 0; 0; en ]
+        ; [ mval; 1; 0; 0; en ]
+        ]
+  in
+  Waveform.print ~wave_width:2 ~display_width:42 ~display_height:24 waves;
+  [%expect
+    {|
+┌Signals─┐┌Waves─────────────────────────┐
+│enable  ││      ┌───────────────────────│
+│        ││──────┘                       │
+│        ││────────────┬─────┬─────┬─────│
+│nums0   ││ 0001       │FFFF │0000 │FFFF │
+│        ││────────────┴─────┴─────┴─────│
+│        ││──────┬─────┬─────┬─────┬─────│
+│nums1   ││ 0001 │0002 │FFFF │0000 │0001 │
+│        ││──────┴─────┴─────┴─────┴─────│
+│        ││──────┬─────┬─────┬───────────│
+│nums2   ││ 0001 │0003 │FFFF │0000       │
+│        ││──────┴─────┴─────┴───────────│
+│        ││──────────────────────────────│
+│nums3   ││ 0000                         │
+│        ││──────────────────────────────│
+│        ││────────────┬─────┬─────┬─────│
+│carry   ││ 0          │2    │0    │1    │
+│        ││────────────┴─────┴─────┴─────│
+│ready   ││      ┌───────────────────────│
+│        ││──────┘                       │
+│        ││──────┬─────┬─────┬───────────│
+│sum     ││ 0000 │0006 │FFFD │0000       │
+│        ││──────┴─────┴─────┴───────────│
+└────────┘└──────────────────────────────┘
 |}]
 ;;
