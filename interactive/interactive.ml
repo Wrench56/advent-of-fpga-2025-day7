d0c3a5b496e5c7693c59673c051f6d7d7cea34ed
Mark Devenyi <markdevenyidev@gmail.com>
2026-01-12 04:15:38 -0500

    feat(interactive): add CLI waveform viewer for testbenches

---
+++ interactive/interactive.ml
@@ -0,0 +1,69 @@
+open Core
+
+module SimulConfig = struct
+  type t =
+    { wave_width : int
+    ; signals_width : int
+    ; values_width : int
+    }
+end
+
+module Testbench = struct
+  type t =
+    { name : string
+    ; description : string
+    ; testbench : unit -> Hardcaml_waveterm.Waveform.t
+    ; config : SimulConfig.t option
+    }
+
+  let all =
+    [ { name = "logic_one"
+      ; description = "AoC 2025 Day 7 Part 1 Simulation"
+      ; testbench = Test.Engine.Test_logic_one.testbench
+      ; config = None
+      }
+    ]
+  ;;
+
+  let all_fmt =
+    all
+    |> List.map ~f:(fun tb -> sprintf " - %-27s %s" tb.name tb.description)
+    |> String.concat ~sep:"\n"
+  ;;
+
+  let find name =
+    match List.find all ~f:(fun t -> String.(t.name = name)) with
+    | Some t -> t
+    | None ->
+      eprintf "Unknown testbench: \"%s\"!\n\nAvailable testbenches:\n%s\n\n" name all_fmt;
+      exit 1
+  ;;
+end
+
+let run_interactive (testbench : Testbench.t) =
+  let config =
+    match testbench.config with
+    | Some conf -> conf
+    | None -> { wave_width = 2; signals_width = 20; values_width = 16 }
+  in
+  let waves = testbench.testbench () in
+  Hardcaml_waveterm_interactive.run
+    ~wave_width:config.wave_width
+    ~signals_width:config.signals_width
+    ~values_width:config.values_width
+    waves
+;;
+
+let testbench_arg : Testbench.t Command.Arg_type.t =
+  Command.Arg_type.create Testbench.find
+;;
+
+let command =
+  Command.basic
+    ~summary:"Run interactive waveform viewer (IAV)"
+    ~readme:(fun () -> sprintf "Available testbenches:\n\n%s\n\n" Testbench.all_fmt)
+    (Command.Param.(map (anon ("testbench" %: testbench_arg))) ~f:(fun testbench () ->
+       run_interactive testbench))
+;;
+
+let () = Command_unix.run ~version:"1.0.0" command
