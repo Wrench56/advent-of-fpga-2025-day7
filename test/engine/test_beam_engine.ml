e9069ac8252bc10555b34d3be783c613c1ddc0c3
Mark Devenyi <markdevenyidev@gmail.com>
2026-01-14 00:24:39 -0500

    test(engine): update `BeamEngine` testbench to emulate removed `SLatch`

--- test/engine/test_beam_engine.ml
+++ test/engine/test_beam_engine.ml
@@ -1,30 +1,29 @@
 open Base
 open Hardcaml
 open Hardcaml_waveterm
-open Solution.Engine.Logic_one
+open Solution.Engine.Beam_engine
 
 let testbench () =
+  let max_iter = 1000 in
   let data_width = 16 in
-  let data_depth = 17 in
+  let data_depth = 16 in
   let mem_fetch_delay = 2 in
   let scope = Scope.create ~flatten_design:true () in
-  let module TestLogicOne =
-    Make_LogicOne (struct
+  let module TestBeamEngine =
+    Make_BeamEngine (struct
       let data_width = data_width
       let data_depth = data_depth
       let max_value = Int.pow 2 32
-      let mem_fetch_delay = mem_fetch_delay
     end)
   in
-  let module Sim = Cyclesim.With_interface (TestLogicOne.I) (TestLogicOne.O) in
-  let sim = Sim.create (TestLogicOne.create scope) in
+  let module Sim = Cyclesim.With_interface (TestBeamEngine.I) (TestBeamEngine.O) in
+  let sim = Sim.create (TestBeamEngine.create scope) in
   let waves, sim = Waveform.create sim in
   let inputs = Cyclesim.inputs sim in
   let outputs = Cyclesim.outputs sim in
   (* Standard example from Advent of Code Day 7 Part 1 *)
   let example_file =
-    [| "################"
-     ; ".......S........"
+    [| ".......S........"
      ; "................"
      ; ".......^........"
      ; "................"
@@ -52,33 +51,37 @@ let testbench () =
       |> Bits.concat_msb)
   in
   let memory = bitrows_of_string example_file in
-  let addresses = Queue.create () in
-  (* Simulate memory fetch delay *)
-  for _ = 1 to mem_fetch_delay do
-    Queue.enqueue addresses 0
-  done;
-  let cycle n =
-    for _ = 1 to n do
-      let addr = Bits.to_int !(outputs.mem_addr) in
-      Queue.enqueue addresses addr;
-      let delayed_addr = Queue.dequeue_exn addresses in
-      inputs.mem_data := memory.(delayed_addr);
-      Utils.cycle sim 1
-    done
+  let cycle n = Utils.cycle sim n in
+  let rec simulate curr_iter curr_slot =
+    if curr_iter >= max_iter
+    then ()
+    else if Bits.equal !(outputs.next_iter_ready) Bits.vdd
+    then (
+      cycle mem_fetch_delay;
+      inputs.mem_data := memory.(curr_slot);
+      inputs.mem_ready := Bits.vdd;
+      cycle 1;
+      inputs.mem_ready := Bits.gnd;
+      simulate (curr_iter + 1) (curr_slot + 1);
+      ())
+    else if Bits.equal !(outputs.solution_ready) Bits.vdd
+    then ()
+    else (
+      cycle 1;
+      simulate (curr_iter + 1) curr_slot)
   in
-  cycle 150;
+  simulate 0 0;
   Stdio.printf
-    "solution_ready = %b; mem_addr = %d; solution = %d\n"
+    "solution_ready = %b; solution = %d\n"
     (Bits.to_bool !(outputs.solution_ready))
-    (Bits.to_int !(outputs.mem_addr))
     (Bits.to_int !(outputs.solution));
   waves
 ;;
 
-let%expect_test "Part 1 Solver Engine works as expected!" =
+let%expect_test "Beam Engine works as expected!" =
   let _ = testbench () in
   [%expect
     {|
-    solution_ready = true; mem_addr = 16; solution = 21
+solution_ready = true; solution = 21
 |}]
 ;;
