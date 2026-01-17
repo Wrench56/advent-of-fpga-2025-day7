86cd1221a36386d87d73b3d96e470edaabea8e51
Mark Devenyi <markdevenyidev@gmail.com>
2026-01-16 23:43:15 -0500

    chore(tools): change ECP5 FPGA type in `verif.sh`

--- tools/verify.sh
+++ tools/verify.sh
@@ -4,5 +4,5 @@ set -e
 
 mkdir -p "artifacts/"
 yosys -p "read_verilog top.v; synth_ecp5 -top top -json artifacts/top.json -abc9"
-nextpnr-ecp5 --json artifacts/top.json --textcfg artifacts/top_out.config --45k --package CABGA256 --report artifacts/top.report --freq 50
+nextpnr-ecp5 --json artifacts/top.json --textcfg artifacts/top_out.config --um-85k --package CABGA381 --report artifacts/top.report --freq 50 --speed 8
 ecppack artifacts/top_out.config artifacts/top.bit
